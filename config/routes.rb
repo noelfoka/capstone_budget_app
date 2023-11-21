ec24a4a78a242dc74df7b2b60b8dd89a35001c21
noelfoka <nomgnenoel@gmail.com>
2023-11-21 01:55:30 +0100

    implement home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'welcome/index'
-  resources :expenses
-  resources :categories
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  authenticated :user do
+    root "categories#index", as: :authenticated_root
+  end
+  root "welcome#index"
+  resources :categories, only: [:index, :new, :show, :create]
+  resources :expenses, only: [:new, :create]
 end
