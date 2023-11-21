88e4f3dbbbfa35554d24a8a0348d678f4afbddc0
noelfoka <nomgnenoel@gmail.com>
2023-11-21 01:22:24 +0100

    make migration

--- db/migrate/20231121001932_create_expenses.rb
+++ db/migrate/20231121001932_create_expenses.rb
@@ -3,7 +3,7 @@ class CreateExpenses < ActiveRecord::Migration[7.1]
     create_table :expenses do |t|
       t.string :name
       t.integer :amount
-      t.references :user, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: { to_table: :users }
       t.references :category, null: false, foreign_key: true
 
       t.timestamps
